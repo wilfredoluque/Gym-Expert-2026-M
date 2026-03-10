1e6fffb4f5b884232c97ebeb1fe6eb76a9bb519d
wilfredo <luquemaytawilfredo@gmail.com>
2026-03-10 13:14:27 -0400

    fix postgres config

--- config/settings.py
+++ config/settings.py
@@ -1,8 +1,5 @@
 import os
-
 class Config:
     SECRET_KEY = "GYM-EXPERT-KEY"
-
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///gym.db"
-
     SQLALCHEMY_TRACK_MODIFICATIONS = False

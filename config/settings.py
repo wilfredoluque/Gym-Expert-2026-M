2ebce5382341da39e3f5f493a72de635a6db65e9
wilfredo <luquemaytawilfredo@gmail.com>
2026-03-10 12:20:23 -0400

    conexion postgres render

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,8 @@
+import os
+
 class Config:
     SECRET_KEY = "GYM-EXPERT-KEY"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///gym.db"
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///gym.db"
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
